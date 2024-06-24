5febfc2a53c2780eb82fc9fe9d9c20d7cc2ba0ff
vikusya_01 <vikakat388@gmail.com>
2024-06-24 22:16:42 +0700

    proverka

---
+++ prover.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+
+
+using namespace std;
+
+int main() {
+
+    vector<vector<double>> A = { {-1.13,-0.04,0.21,-18},
+    {0.25,-1.23,0.14,-0.09},
+    {-0.21,0.14,0.8,-0.13},
+    {0.15,-1.31,0.06,0.87} };
+
+    vector<double> b = { -1.24, 0.87, 2.56, -1.13 };
+
+    cout << setw(1) << "N"
+        << setw(10) << "x1"
+        << setw(10) << "x2"
+        << setw(10) << "x3"
+        << setw(10) << "x4"<< endl;
+
+
+    
+
+
+    return 0;
+}
+
